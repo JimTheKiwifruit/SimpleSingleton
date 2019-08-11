14ca6072b9dffcfbcf45ede048b8d8f163321e2b
James Penman <James.Pen@live.com>
2019-08-11 16:29:08 +1200

    Added Singleton.cs ready for v1

---
+++ Runtime/Singleton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JimTheKiwifruit.Singleton {
+
+    public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+
+        static T instance;
+        public static T Instance {
+            get {
+                if (!instance)
+                    instance = FindObjectOfType<T>();
+
+                if (!instance)
+                    instance = new GameObject(typeof(T).Name).AddComponent<T>();
+
+                return instance;
+            }
+        }
+
+        protected virtual void Awake() {
+            var dis = this as T;
+            if (instance != null && instance != dis) {
+                Destroy(gameObject);
+                return;
+            }
+
+            instance = dis;
+        }
+
+    }
+
+}
